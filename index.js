c45e4690939af0711c44668310002b1b9b12b1ea
Cameron <quiksilver980@gmail.com>
2018-03-19 23:03:28 -0700

    Added Topic functionaily, fully linted and beautified

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var http = require("http");
 var fs = require("fs");
 var extract = require("./extract");
-var wss = require("./websockets-server");
-
+var wss = require("./websockets-server"); /*eslint no-unused-vars: 0*/
 var mime = require("mime");
 
-var handleError = function (err, res) {
+var handleError = function(err, res) {
   fs.readFile("app/error.html", "utf-8", function(err, page) {
-    res.writeHead(404, {"Content-Type": "text/html"});
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    });
     res.write(page);
     res.end();
   });
@@ -18,10 +19,9 @@ var server = http.createServer(function(req, res) {
 
   var filePath = extract(req.url);
 
-  //var type = mime.getType(filePath);
   res.setHeader("Content-Type", mime.getType(filePath));
 
-  fs.readFile(filePath, function (err, data) {
+  fs.readFile(filePath, function(err, data) {
     if (err) {
       handleError(err, res);
       return;
--- websockets-server.js
+++ websockets-server.js
@@ -5,21 +5,36 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
+var topic = "";
 
 console.log("websockets server started");
 
-ws.on("connection", function (socket) {
+ws.on("connection", function(socket) {
   console.log("client connection established");
 
-  messages.forEach(function (msg) {
+  if (topic != "")
+    socket.send("*** Topic is '" + topic + "'");
+
+  messages.forEach(function(msg) {
     socket.send(msg);
   });
 
-  socket.on("message", function (data) {
-    console.log("message received: " + data);
-    messages.push(data);
-    ws.clients.forEach(function (clientSocket) {
-      clientSocket.send(data);
-    });
+  socket.on("message", function(data) {
+
+    if (data.startsWith("/topic")) {
+
+      if (data.slice(7) != "") {
+        topic = data.slice(7);
+
+        ws.clients.forEach(function(clientSocket) {
+          clientSocket.send("*** Topic has changed to '" + topic + "'");
+        });
+      }
+    } else {
+      console.log("message received: " + data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(data);
+      });
+    }
   });
 });
